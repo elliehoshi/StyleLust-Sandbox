7a2984fad3354fecd7952c2127928aec2f56dad4
ElenaHoshizaki <elena.hoshizaki@gmail.com>
2014-02-20 17:35:33 -0800

    first commit

---
+++ stylelust/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stylelust::Application.config.secret_key_base = '85c4108c474cdaaa874097b3964e215ed695198fe7b398fa1663801d2d7bff4f381032b3c0af6b74b148ef188728243031f0073481a584c8fd6902a9f25a0470'
