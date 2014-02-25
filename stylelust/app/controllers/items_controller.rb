398a9d975b16bac4b7c892d5101546070b23d0b0
ElenaHoshizaki <elena.hoshizaki@gmail.com>
2014-02-24 18:46:08 -0800

    create custom params - thanks Kendrick

--- stylelust/app/controllers/items_controller.rb
+++ stylelust/app/controllers/items_controller.rb
@@ -1,5 +1,13 @@
 class ItemsController < ApplicationController
 	def play
-		
+		if params[:cat].blank?
+			@items = Item.all
+		else
+			if params[:price].blank?
+				@items = Category.find_by(name: params[:cat]).items
+			else
+				@items = Category.find_by(name: params[:cat]).items.where("price < ?", params[:price].to_f)
+			end
+		end		
 	end
 end
